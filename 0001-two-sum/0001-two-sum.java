9e41ecd24a683712fbef5ff449ab466600c4b06d
Sagar Raj <161850830+SgrRaj@users.noreply.github.com>
2026-09-26 16:00:32 +0530

    Time: 44 ms (34.55%), Space: 47.1 MB (59.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,33 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-          Map<Integer,Integer> m=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            int comp=target-nums[i];
-            int index=m.getOrDefault(comp,-1);
-            if(index>=0){
-                return new int[] {i,index};
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
             }
-              m.put(nums[i],i);
-          
         }
-          return new int[] {-1,-1};
+        return new int[]{};
 
-
-          
-        // for(int i=0;i<nums.length;i++){
-        //     for(int j=i+1;j<nums.length;j++){
-        //         if(nums[i]+nums[j]==target){
-        //             return new int[]{i,j};
-        //         }
-        //     }
-        // }
-        // return new int[]{};
-    }
+  }
     
 }
+
+
+
+
+
+
+
+        //  Map<Integer,Integer> m=new HashMap<>();
+        // for(int i=0;i<nums.length;i++){
+        //     int comp=target-nums[i];
+        //     int index=m.getOrDefault(comp,-1);
+        //     if(index>=0){
+        //         return new int[] {i,index};
+        //     }
+        //       m.put(nums[i],i);
+          
+        // }
+        //   return new int[] {-1,-1};
+  
